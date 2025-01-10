ac6234a6cdb02bc463bc4abc6c2e050eaa597b68
Danny Hyatt <dannyhyatt@Dannys-MacBook-Pro.local>
2025-01-10 12:28:02 -0800

    [feature] add Vue component directory

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,18 @@
 import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
+import { path } from '@vuepress/utils'
+import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   bundler: viteBundler(),
   theme: defaultTheme(),
-  base: '/vue-planner/'
+  base: '/vue-planner/',
+  plugins: [
+    [
+      registerComponentsPlugin({
+        componentsDir: path.resolve(__dirname, './components'),
+      }),
+    ],
+  ],
 })
